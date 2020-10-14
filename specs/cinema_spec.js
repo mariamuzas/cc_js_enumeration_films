a5e73ec5a427ece17cb66d74ec001b57a24a9196
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-14 14:51:09 +0100

    added test for films by genre

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -38,7 +38,10 @@ describe('Cinema', function () {
     assert.strictEqual(actual, trainspotting)
   });
 
-  it('should be able to filter films by genre');
+  it('should be able to filter films by genre', function () {
+    const actual = cinema.filmsByGenre("drama");
+    assert.deepStrictEqual(actual, [moonlight, trainspotting]);
+  });
   it('should be able to check whether there are some films from a particular year');
   it('should be able to check whether there are no films from a particular year');
   it('should be able to check whether all films are over a particular length');
