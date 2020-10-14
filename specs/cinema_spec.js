bdacae72ea3b5eb24cf451cfd6915d356806f046
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-14 14:57:36 +0100

    added test for films by year

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -42,7 +42,13 @@ describe('Cinema', function () {
     const actual = cinema.filmsByGenre("drama");
     assert.deepStrictEqual(actual, [moonlight, trainspotting]);
   });
-  it('should be able to check whether there are some films from a particular year');
+  
+  it('should be able to check whether there are some films from a particular year', function () {
+    const actual = cinema.filmsByYear(2017);
+    assert.deepStrictEqual(actual, [bladeRunner, dunkirk, trainspotting]);
+  });
+
+
   it('should be able to check whether there are no films from a particular year');
   it('should be able to check whether all films are over a particular length');
   it('should be able to calculate total running time of all films');
